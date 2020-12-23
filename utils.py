08f7eb041df19a00555c9da1b700060eb59d206b
Nauman Mustafa <nauman.mustafa.x@gmail.com>
2020-12-23 11:39:39 +0500

    add black formating

--- utils.py
+++ utils.py
@@ -2,14 +2,16 @@ import random
 import numpy as np
 import torch
 
-def set_seed(seed):     # set the random seed for reproducibility
+
+def set_seed(seed):  # set the random seed for reproducibility
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
 
+
 def strip_eos(sents):
-    return [sent[:sent.index('<eos>')] if '<eos>' in sent else sent
-        for sent in sents]
+    return [sent[: sent.index("<eos>")] if "<eos>" in sent else sent for sent in sents]
+
 
 def load_sent(path):
     sents = []
@@ -18,44 +20,51 @@ def load_sent(path):
             sents.append(line.split())
     return sents
 
+
 def write_sent(sents, path):
-    with open(path, 'w') as f:
+    with open(path, "w") as f:
         for s in sents:
-            f.write(' '.join(s) + '\n')
+            f.write(" ".join(s) + "\n")
+
 
 def write_doc(docs, path):
-    with open(path, 'w') as f:
+    with open(path, "w") as f:
         for d in docs:
             for s in d:
-                f.write(' '.join(s) + '\n')
-            f.write('\n')
+                f.write(" ".join(s) + "\n")
+            f.write("\n")
+
 
 def write_z(z, path):
-    with open(path, 'w') as f:
+    with open(path, "w") as f:
         for zi in z:
             for zij in zi:
-                f.write('%f ' % zij)
-            f.write('\n')
+                f.write("%f " % zij)
+            f.write("\n")
+
 
 def logging(s, path, print_=True):
     if print_:
         print(s)
     if path:
-        with open(path, 'a+') as f:
-            f.write(s + '\n')
+        with open(path, "a+") as f:
+            f.write(s + "\n")
+
 
 def lerp(t, p, q):
-    return (1-t) * p + t * q
+    return (1 - t) * p + t * q
+
 
 # spherical interpolation https://github.com/soumith/dcgan.torch/issues/14#issuecomment-199171316
 def slerp(t, p, q):
-    o = np.arccos(np.dot(p/np.linalg.norm(p), q/np.linalg.norm(q)))
+    o = np.arccos(np.dot(p / np.linalg.norm(p), q / np.linalg.norm(q)))
     so = np.sin(o)
-    return np.sin((1-t)*o) / so * p + np.sin(t*o) / so * q
+    return np.sin((1 - t) * o) / so * p + np.sin(t * o) / so * q
+
 
 def interpolate(z1, z2, n):
     z = []
     for i in range(n):
-        zi = lerp(1.0*i/(n-1), z1, z2)
+        zi = lerp(1.0 * i / (n - 1), z1, z2)
         z.append(np.expand_dims(zi, axis=0))
     return np.concatenate(z, axis=0)
