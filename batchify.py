df104c70daaa8a9f1e414618e8f13ae4f8a01085
Nauman Mustafa <nauman.mustafa.x@gmail.com>
2020-12-23 11:52:04 +0500

    add black formating

--- batchify.py
+++ batchify.py
@@ -1,5 +1,6 @@
 import torch
 
+
 def get_batch(x, vocab, device):
     go_x, x_eos = [], []
     max_len = max([len(s) for s in x])
@@ -8,8 +9,12 @@ def get_batch(x, vocab, device):
         padding = [vocab.pad] * (max_len - len(s))
         go_x.append([vocab.go] + s_idx + padding)
         x_eos.append(s_idx + [vocab.eos] + padding)
-    return torch.LongTensor(go_x).t().contiguous().to(device), \
-           torch.LongTensor(x_eos).t().contiguous().to(device)  # time * batch
+    return torch.LongTensor(go_x).t().contiguous().to(device), torch.LongTensor(
+        x_eos
+    ).t().contiguous().to(
+        device
+    )  # time * batch
+
 
 def get_batches(data, vocab, batch_size, device):
     order = range(len(data))
@@ -20,8 +25,8 @@ def get_batches(data, vocab, batch_size, device):
     i = 0
     while i < len(data):
         j = i
-        while j < min(len(data), i+batch_size) and len(data[j]) == len(data[i]):
+        while j < min(len(data), i + batch_size) and len(data[j]) == len(data[i]):
             j += 1
-        batches.append(get_batch(data[i: j], vocab, device))
+        batches.append(get_batch(data[i:j], vocab, device))
         i = j
     return batches, order
